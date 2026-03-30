6f83b971149d774ba2d175d33fc5337ae5dbb03c
gasejaca-star <gasejaca@gmail.com>
2026-03-30 11:23:57 -0500

    Update srilinea.py

--- srilinea.py
+++ srilinea.py
@@ -95,7 +95,7 @@ if not st.session_state.es_premium and st.session_state.invitaciones_disponibles
         st.subheader("👑 Versión Premium")
         with st.expander("💎 VER VALOR Y DATOS DE PAGO", expanded=True):
             st.markdown(f"""
-            ### 💰 Costo: $20 / Año
+            ### 💰 Costo: $20 / AÑO O $2.99 / MES
             **Transferencia Bancaria (Ecuador):**
             * **Banco:** Banco Pichincha (Ahorros) 2205082283
             * **Beneficiario:** Gabriel  Jácome 
